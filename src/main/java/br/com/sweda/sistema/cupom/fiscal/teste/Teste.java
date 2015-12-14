61ce780bf45d51f71a588981b0d851ee5cb44e4b
lvarga <lvarga@Sweda17.sweda.sp>
2015-12-14 18:00:20 -0200

    teste

---
+++ src/main/java/br/com/sweda/sistema/cupom/fiscal/teste/Teste.java
@@ -0,0 +1,21 @@
+package br.com.sweda.sistema.cupom.fiscal.teste;
+
+import java.util.Random;
+
+import br.com.sweda.sistema.cupom.fiscal.integration.sat.SAT;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		
+		/*public String ConsultarSAT(int numeroSessao) {
+			
+			Random gerador = new Random();
+			
+			int numeroSessao = gerador.nextInt();
+			
+		}*/
+
+	}
+
+}
