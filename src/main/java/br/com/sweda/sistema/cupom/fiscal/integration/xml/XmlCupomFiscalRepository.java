1497f638341e76ac2041a7f3900f1357b2f47cb5
salespaulo <salespaulo08@gmail.com>
2015-12-14 20:45:03 -0200

    ...

--- src/main/java/br/com/sweda/sistema/cupom/fiscal/integration/xml/XmlCupomFiscalRepository.java
+++ src/main/java/br/com/sweda/sistema/cupom/fiscal/integration/xml/XmlCupomFiscalRepository.java
@@ -1,7 +1,9 @@
 package br.com.sweda.sistema.cupom.fiscal.integration.xml;
 
+import br.com.sweda.sistema.cupom.fiscal.integration.xml.tags.CFe;
 import br.com.sweda.sistema.cupom.fiscal.model.CupomFiscal;
 import br.com.sweda.sistema.cupom.fiscal.model.repository.CupomFiscalRepository;
+import com.thoughtworks.xstream.XStream;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.io.Serializable;
@@ -16,12 +18,20 @@ public class XmlCupomFiscalRepository implements CupomFiscalRepository {
 
     @Override
     public CupomFiscal carregar(Serializable id) {
-        // TODO carregar o XML
-        return null;
+        XStream xstream = new XStream();
+        CFe cfe = (CFe) xstream.fromXML((String) id);
+        CupomFiscal cupomFiscal = new CupomFiscal();
+        cupomFiscal.setCfe(cfe);
+        return cupomFiscal;
     }
 
     @Override
     public void salvar(CupomFiscal cupomFiscal) {
         throw new NotImplementedException();
+    }
+
+    public static void main(String ...a) {
+        CupomFiscal cupomFiscal = new XmlCupomFiscalRepository().carregar("cupom_fiscal.xml");
+        System.out.println(cupomFiscal);
     }
 }
--- src/main/java/br/com/sweda/sistema/cupom/fiscal/model/CupomFiscal.java
+++ src/main/java/br/com/sweda/sistema/cupom/fiscal/model/CupomFiscal.java
@@ -1,5 +1,16 @@
 package br.com.sweda.sistema.cupom.fiscal.model;
 
+import br.com.sweda.sistema.cupom.fiscal.integration.xml.tags.CFe;
+
 public class CupomFiscal {
 
+    private CFe cfe;
+
+    public CFe getCfe() {
+        return cfe;
+    }
+
+    public void setCfe(CFe cfe) {
+        this.cfe = cfe;
+    }
 }
